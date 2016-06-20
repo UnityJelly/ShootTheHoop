624974e5837f1b4318903575b0a049e33fcad976
marc carlyon <irresistiblejelly@gmail.com>
2016-06-20 20:12:52 +0100

    L35 Using Multiple scenes

---
+++ Shoot The Hoop/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+public class ScoreKeeper : MonoBehaviour {
+
+    // Use this for initialization
+    int score = 0;
+
+void Start () {
+	
+	}
+	
+	
+// Update is called once per frame
+	
+void Update () {
+	
+	}
+
+public void IncrementScore(int amount)
+    {
+        score += amount;
+        print("You Scored. Your current score is " + score);       
+    }
+}
