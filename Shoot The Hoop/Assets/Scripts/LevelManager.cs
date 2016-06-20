f406a2307db373b6f89ecc4077a9b21b65b22b2f
marc carlyon <irresistiblejelly@gmail.com>
2016-06-20 20:39:33 +0100

    L36 Creating Menu Screen UI

---
+++ Shoot The Hoop/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour {
+
+	    // Use this for initialization
+	
+    void Start () {
+
+	}
+	
+	
+        // Update is called once per frame
+	
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadNextScene();
+        }
+    }
+
+    private static void LoadNextScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+}
